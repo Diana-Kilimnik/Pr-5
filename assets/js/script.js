3d170e2e2e0e638cc636cd3acc655ce482de957a
unknown <kylymnyk.n@gmail.com>
2020-10-25 21:55:11 +0300

    pr6

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+var slideIndex = 0;
+var currentSlideIndex = 0;
+var slideArray = [];
+
+function Slide(title, subtitle, background, link ) {
+   this.title = title;
+   this.subtitle = subtitle;
+   this.background = background;
+   this.link = link;
+   this.id = "slide" + slideIndex;
+   slideIndex++;
+   slideArray.push(this);
+}
+
+var slide1 = new Slide(
+   "Nasa",
+   "National Aeronautics and Space Administration",
+   "images/img.png",
+   "https://www.nasa.gov/"
+);
+
+var slide2 = new Slide(
+   "Wikipedia",
+   "Federal Government responsible for the civilian space program, <br \/> as well as aeronautics and space research.",
+   "images/img1.png",
+   "https://en.wikipedia.org/wiki/NASA"
+);
+
+var slide3 = new Slide(
+   "Twitter",
+   "Explore the universe and our home planet with NASA 🌎 <br \/> We usually post in EDT.",
+   "images/img2.png",
+   "https://twitter.com/nasa?lang=uk"
+);
+
+function buildSlider(){
+   var myHTML;
+   for(var i = 0; i < slideArray.length; i++) {
+       myHTML += "<div id='" + slideArray[i].id +
+           "' class='singleSlide' style='background-image:url(" + slideArray[i].background + ");'>" +
+           "<div class='slideOverlay'>" +
+           "<h1>" + slideArray[i].title + "</h1>" +
+           "<h4>" + slideArray[i].subtitle + "</h4>" +
+           "<a class='slider' href='" + slideArray[i].link + "' target='_blank'>Open Link</a>" +
+           "</div>" +
+           "</div>";
+   }
+
+   document.getElementById("mySlider").innerHTML = myHTML;
+   document.getElementById("slide" + currentSlideIndex).style.left = 0;
+}
+
+buildSlider();
+
+function prevSlide(){
+   var nextSlideIndex;
+   if (currentSlideIndex === 0 ) {
+       nextSlideIndex = slideArray.length - 1;
+   } else {
+       nextSlideIndex = currentSlideIndex - 1;
+   }
+
+   document.getElementById("slide" + nextSlideIndex).style.left = "-100%";
+   document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+   document.getElementById("slide" + nextSlideIndex).setAttribute("class", "singleSlide slideInLeft");
+   document.getElementById("slide" + currentSlideIndex).setAttribute("class", "singleSlide slideOutRight");
+
+   currentSlideIndex = nextSlideIndex;
+}
+
+function nextSlide(){
+   var nextSlideIndex;
+   if (currentSlideIndex === (slideArray.length - 1) ) {
+       nextSlideIndex = 0;
+   } else {
+       nextSlideIndex = currentSlideIndex + 1;
+   }
+
+   document.getElementById("slide" + nextSlideIndex).style.left = "100%";
+   document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+   document.getElementById("slide" + nextSlideIndex).setAttribute("class", "singleSlide slideInRight");
+   document.getElementById("slide" + currentSlideIndex).setAttribute("class", "singleSlide slideOutLeft");
+
+   currentSlideIndex = nextSlideIndex;
+}
